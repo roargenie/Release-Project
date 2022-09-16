1a7dce553acb108d751e0d2cae4b71f3446fac5c
roargenie <105975078+roargenie@users.noreply.github.com>
2022-09-16 10:52:35 +0900

    ✅Realm구성중

--- Release Project/View/AddItem/AddItemDetailView.swift
+++ Release Project/View/AddItem/AddItemDetailView.swift
@@ -5,147 +5,19 @@ import UIKit
 
 final class AddItemDetailView: BaseView {
     
-    private let imageView: UIImageView = {
+    let collectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout())
+        view.register(FirstAddItemDetailCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: FirstAddItemDetailCollectionReusableView.identifier)
+        view.register(SecondAddItemDetailCollectionViewCell.self, forCellWithReuseIdentifier: SecondAddItemDetailCollectionViewCell.reuseIdentifier)
+        view.register(ThirdAddItemDetailCollectionViewCell.self, forCellWithReuseIdentifier: ThirdAddItemDetailCollectionViewCell.reuseIdentifier)
+        
+        return view
+    }()
+    
+    let imageView: UIImageView = {
         let view = UIImageView()
         view.contentMode = .scaleToFill
         view.backgroundColor = .lightGray
-        return view
-    }()
-    
-    // MARK: - 카테고리 태그 버튼
-    
-    private let categoryLabel: UILabel = {
-        let view = UILabel()
-        view.text = "카테고리"
-        return view
-    }()
-    
-    private let outerTagButton: TagButton = {
-        let view = TagButton()
-        view.setTitle("Outer", for: .normal)
-        return view
-    }()
-    
-    private let topTagButton: TagButton = {
-        let view = TagButton()
-        view.setTitle("Top", for: .normal)
-        return view
-    }()
-    
-    private let bottomTagButton: TagButton = {
-        let view = TagButton()
-        view.setTitle("Bottom", for: .normal)
-        return view
-    }()
-    
-    private let shoesTagButton: TagButton = {
-        let view = TagButton()
-        view.setTitle("Shoes", for: .normal)
-        return view
-    }()
-    
-    private let accTagButton: TagButton = {
-        let view = TagButton()
-        view.setTitle("Acc", for: .normal)
-        return view
-    }()
-    
-    private let otherTagButton: TagButton = {
-        let view = TagButton()
-        view.setTitle("Other", for: .normal)
-        return view
-    }()
-    
-    private lazy var categoryStackView1: UIStackView = {
-        let view = UIStackView(arrangedSubviews: [outerTagButton, topTagButton, bottomTagButton, shoesTagButton])
-        view.axis = .horizontal
-        view.spacing = 10
-        view.alignment = .fill
-        view.distribution = .fillEqually
-        return view
-    }()
-    
-    private lazy var categoryStackView2: UIStackView = {
-        let view = UIStackView(arrangedSubviews: [accTagButton, otherTagButton])
-        view.axis = .horizontal
-        view.spacing = 10
-        view.alignment = .fill
-        view.distribution = .fillEqually
-        return view
-    }()
-    
-    // MARK: - 계절 태그 버튼
-    
-    private let seasonLabel: UILabel = {
-        let view = UILabel()
-        view.text = "계절"
-        return view
-    }()
-    
-    private let springTagButton: TagButton = {
-        let view = TagButton()
-        view.setTitle("Spring", for: .normal)
-        return view
-    }()
-    
-    private let summerTagButton: TagButton = {
-        let view = TagButton()
-        view.setTitle("Summer", for: .normal)
-        return view
-    }()
-    
-    private let autumnTagButton: TagButton = {
-        let view = TagButton()
-        view.setTitle("Fall", for: .normal)
-        return view
-    }()
-    
-    private let winterTagButton: TagButton = {
-        let view = TagButton()
-        view.setTitle("Winter", for: .normal)
-        return view
-    }()
-    
-    private lazy var seasonStackView: UIStackView = {
-        let view = UIStackView(arrangedSubviews: [springTagButton, summerTagButton, autumnTagButton, winterTagButton])
-        view.axis = .horizontal
-        view.spacing = 10
-        view.alignment = .fill
-        view.distribution = .fillEqually
-        return view
-    }()
-    
-    // MARK: - 구매한 목록에 표시할지 여부
-    
-    private let amountLabel: UILabel = {
-        let view = UILabel()
-        view.text = "구매가"
-        return view
-    }()
-    
-    private let purchaseCheckBox: UIButton = {
-        let view = UIButton()
-        view.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
-        return view
-    }()
-    
-    private let amountTextField: UITextField = {
-        let view = UITextField()
-        view.placeholder = "상품가격을 입력해주세요"
-        return view
-    }()
-    
-    let cancelButton: UIButton = {
-        let view = UIButton()
-        view.setTitle("취소", for: .normal)
-        view.setTitleColor(UIColor.black, for: .normal)
-        return view
-    }()
-    
-    let doneButton: UIButton = {
-        let view = UIButton()
-        view.setTitle("저장", for: .normal)
-        view.setTitleColor(UIColor.black, for: .normal)
         return view
     }()
     
@@ -158,73 +30,31 @@ final class AddItemDetailView: BaseView {
     }
     
     override func configureUI() {
-        [imageView, categoryLabel, categoryStackView1, categoryStackView2, seasonLabel, seasonStackView, amountLabel, purchaseCheckBox, amountTextField, doneButton, cancelButton].forEach { self.addSubview($0) }
+        [collectionView, imageView].forEach { self.addSubview($0) }
     }
     
     override func setConstraints() {
-        cancelButton.snp.makeConstraints { make in
-            make.top.equalTo(self.safeAreaLayoutGuide).offset(8)
-            make.leading.equalTo(self.safeAreaLayoutGuide).offset(12)
-            make.width.height.equalTo(50)
-        }
-        doneButton.snp.makeConstraints { make in
-            make.centerY.equalTo(cancelButton.snp.centerY)
-            make.trailing.equalTo(self.safeAreaLayoutGuide).offset(-12)
-            make.width.height.equalTo(50)
-        }
-        
         imageView.snp.makeConstraints { make in
-            make.top.equalTo(cancelButton.snp.bottom).offset(16)
-            make.leading.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
+            make.top.leading.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
             make.height.equalTo(self.snp.height).multipliedBy(0.3)
         }
-        categoryLabel.snp.makeConstraints { make in
-            make.top.equalTo(imageView.snp.bottom).offset(30)
-            make.leading.equalTo(imageView.snp.leading)
-            make.height.equalTo(30)
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(20)
+            make.leading.trailing.bottom.equalTo(self.safeAreaLayoutGuide)
         }
-        categoryStackView1.snp.makeConstraints { make in
-            make.top.equalTo(categoryLabel.snp.bottom).offset(12)
-            make.leading.equalTo(self.safeAreaLayoutGuide).offset(20)
-            make.trailing.equalTo(self.safeAreaLayoutGuide).offset(-20)
-            make.height.equalTo(30)
-        }
-        categoryStackView2.snp.makeConstraints { make in
-            make.top.equalTo(categoryStackView1.snp.bottom).offset(10)
-            make.leading.equalTo(categoryStackView1.snp.leading)
-            make.trailing.equalTo(topTagButton.snp.trailing)
-            make.height.equalTo(30)
-        }
-        seasonLabel.snp.makeConstraints { make in
-            make.top.equalTo(categoryStackView2.snp.bottom).offset(16)
-            make.leading.equalTo(categoryLabel.snp.leading)
-            make.height.equalTo(30)
-        }
-        seasonStackView.snp.makeConstraints { make in
-            make.top.equalTo(seasonLabel.snp.bottom).offset(12)
-            make.leading.equalTo(self.safeAreaLayoutGuide).offset(20)
-            make.trailing.equalTo(self.safeAreaLayoutGuide).offset(-20)
-            make.height.equalTo(30)
-        }
-        amountLabel.snp.makeConstraints { make in
-            make.top.equalTo(seasonStackView.snp.bottom).offset(16)
-            make.leading.equalTo(categoryLabel.snp.leading)
-            make.height.equalTo(30)
-            make.width.equalTo(60)
-        }
-        purchaseCheckBox.snp.makeConstraints { make in
-            make.centerY.equalTo(amountLabel.snp.centerY)
-            make.leading.equalTo(amountLabel.snp.trailing).offset(12)
-            make.width.height.equalTo(25)
-        }
-        amountTextField.snp.makeConstraints { make in
-            make.top.equalTo(amountLabel.snp.bottom).offset(16)
-            make.leading.equalTo(self.safeAreaLayoutGuide).offset(20)
-            make.trailing.equalTo(self.safeAreaLayoutGuide).offset(-20)
-            make.height.equalTo(30)
-        }
-
     }
     
-    
+    static func collectionViewLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        let deviceWidth: CGFloat = UIScreen.main.bounds.width
+        let itemSize: CGFloat = (deviceWidth - 50) / 4
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 0
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        layout.itemSize = CGSize(width: itemSize, height: 50)
+        layout.headerReferenceSize = CGSize(width: deviceWidth, height: 75)
+        layout.scrollDirection = .vertical
+        return layout
+    }
 }
